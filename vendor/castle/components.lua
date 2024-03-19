5acb85a81a49fb9e8321d3de2180599e1e687a18
David Crosby <dcrosby42@gmail.com>
2024-03-18 22:16:19 -0400

    delete unused code

--- vendor/castle/components.lua
+++ vendor/castle/components.lua
@@ -1,4 +1,3 @@
--- require 'castle.helpers'
 local Comp = require 'castle.ecs.component'
 
 local ext = appendlist
@@ -39,29 +38,10 @@ Comp.define("circle", { 'offx', 0, 'offy', 0, 'radius', 0, 'fill', true, 'color'
 Comp.define("rect", { 'offx', 0, 'offy', 0, 'w', 0, 'h', 0, 'color', { 1, 1, 1 }, 'style', 'fill', 'draw', true,
   'debugonly', false })
 
--- Comp.define("text", {
---   'text', 'Words',
---   'color', { 0, 0, 0 },
---   'font', '',
---   'width', 0,
---   'align', 'left',
---   'height', 0,
---   'valign', 'middle',
---   'r', 0,
---   'offx', 0,
---   'offy', 0,
---   'debugonly', false,
---   'shadowcolor', false,
---   'shadowx', 0,
---   'shadowy', 0,
---   'debugdraw', false })
-
---
 Comp.define("pos", { 'x', 0, 'y', 0, 'r', 0 })
 Comp.define("scale", { 'x', 1, 'y', 1 })
 Comp.define("rot", { 'r', 0, 'aboutx', 0, 'abouty', 0 })
 Comp.define("bounds", { 'offx', 0, 'offy', 0, 'w', 0, 'h', 0 })
---
 
 Comp.define("vel", { 'dx', 0, 'dy', 0, 'angularvelocity', 0, 'lineardamping', 0, 'angulardamping', 0 })
 
@@ -71,7 +51,6 @@ Comp.define("bgcolor", { 'color', { 0, 0, 0 } })
 
 Comp.define("timer", { 't', 0, 'factor', 1, 'reset', 0, 'countDown', true, 'loop', false, 'alarm', false, 'event', '' })
 
--- Comp.define("controller", {'id','','leftx',0,'lefty',0,})
 Comp.define("controller", { 'id', '' })
 
 Comp.define("viewport", { 'x', 0, 'y', 0, 'sx', 1, 'sy', 1 })
@@ -164,6 +143,11 @@ Comp.define("manipulator", { 'id', '', 'mode', '', 'x', 0, 'y', 0, 'dx', 0, 'dy'
 Comp.define("screen_grid", { 'spacex', 10, 'spacey', '', 'color', { 1, 1, 1, 0.3 } })
 
 Comp.define("health", { 'hp', 10, 'maxhp', 10 })
+
+
+--
+-- DELETEME
+--
 
 
 
