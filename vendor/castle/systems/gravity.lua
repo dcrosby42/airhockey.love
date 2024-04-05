19e8994c74f105cda0fe19449ff85d67a0311581
David Crosby <dcrosby42@gmail.com>
2024-04-05 16:44:07 -0400

    Removing old castle systems

--- vendor/castle/systems/gravity.lua
+++
@@ -1,7 +0,0 @@
-
-return defineUpdateSystem(
-  {'pos'}, 
-  function(e, estore,input,res)
-    e.pos.y = e.pos.y + 1
-  end
-)
--- vendor/castle/systems/posmover.lua
+++
@@ -1,9 +0,0 @@
-
-return function(estore, input,res)
-  estore:search(
-    hasComps('controller', 'pos'),
-    function(e)
-      e.pos.x = e.pos.x + (600 * e.controller.leftx * input.dt)
-    end
-  )
-end
